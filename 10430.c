69f4fb13ece0586a2191ee22361cdfb323e25d64
starboard98 <55227905+starboard98@users.noreply.github.com>
2021-03-21 02:18:02 +0900

    Create 10430.c

---
+++ 10430.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int A, B, C;
+
+	scanf("%d %d %d", &A, &B, &C);
+	printf("%d\n%d\n%d\n%d\n", (A+B)%C, ((A%C)+(B+C))%C, (A*B)%C, ((A%C)*(B%C))%C);
+
+	return 0;
+}
