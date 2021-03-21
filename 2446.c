f71d727c505c85f014d024ea1696e1dcf69159dd
starboard98 <55227905+starboard98@users.noreply.github.com>
2021-03-21 14:13:06 +0900

    Create 2446.c

---
+++ 2446.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int N;
+
+	scanf("%d", &N);
+
+	for(int i=N;i>0;i--)
+	{
+		for(int j=0;i+j<N;j++)
+			printf(" ");
+		printf("*");
+		for(int j=1;j<i;j++)
+			printf("**");
+		printf("\n");
+	}
+	for(int i=2;i<=N;i++)
+	{
+		for(int j=0;i+j<N;j++)
+			printf(" ");
+		printf("*");
+		for(int j=1;j<i;j++)
+			printf("**");
+		printf("\n");
+	}
+
+	return 0;
+}
